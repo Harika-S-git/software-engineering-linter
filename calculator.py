4d265b03bcb1eaac0c4fde8287605da87dc61dfb
Harika S <haarikaa.s07@gmail.com>
2026-09-09 09:28:36 +0530

    Fix code formatting

--- calculator.py
+++ calculator.py
@@ -1,16 +1,12 @@
-def add(a, b):
-    return a + b
+def add(a,b):
+    return a+b
 
+def subtract(a,b):
+    return a-b
 
-def subtract(a, b):
-    return a - b
+def multiply(a,b):
+    return a*b
 
-
-def multiply(a, b):
-    return a * b
-
-
-print(add(10, 5))
-
-print(subtract(10, 5))
-print(multiply(10, 5))
+print(add(10,5))
+print(subtract(10,5))
+print(multiply(10,5))
