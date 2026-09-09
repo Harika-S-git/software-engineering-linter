92e4ec69513c397e6b504148362231b96a8b6567
Harika S <haarikaa.s07@gmail.com>
2026-09-09 09:49:35 +0530

    Fix missing newline

--- calculator.py
+++ calculator.py
@@ -1,12 +1,16 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
-def subtract(a,b):
-    return a-b
 
-def multiply(a,b):
-    return a*b
+def subtract(a, b):
+    return a - b
 
-print(add(10,5))
-print(subtract(10,5))
-print(multiply(10,5))
+
+def multiply(a, b):
+    return a * b
+
+
+print(add(10, 5))
+
+print(subtract(10, 5))
+print(multiply(10, 5))
